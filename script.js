c2e40d49af6c6415dd7b97c4034890dcf59e5846
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-11-18 17:51:49 +1300

    implementing to localStorage

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-const inputBtn = document.getElementById("input-btn")
+// chrome://extensions/
 let myLeads = []
+
+const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-inputBtn.addEventListener("click", function() {
-  // Push the value from the inputEl into the myLeads array 
-  myLeads.push(inputEl.value)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
   renderLeads()
+}
+
+//INPUT BUTTON
+inputBtn.addEventListener("click", function() {
+
+  myLeads.push(inputEl.value)
   inputEl.value = ""
+  // Save the myLeads array to localStorage 
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  renderLeads()
+  //Verifying
+  console.log( localStorage.getItem("myLeads") )
 }) 
 
-// 1. Create a variable, listItems, to hold all the HTML for the list items
+
+//DELETE BUTTON - listening for double clicks
+deleteBtn.addEventListener("dblclick", function() {
+  console.log("double clicked")
+  //When clicked, clear localStorage, myLeads, and the DOM
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+
+// Create a variable, listItems, to hold all the HTML for the list items
 function renderLeads() {
   let listItems = ""
 
   for(let i = 0; i < myLeads.length; i++) {
-      // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+     
     listItems += `
       <li>
         <a target='_blank' href='${myLeads[i]}'>
@@ -23,6 +48,6 @@ function renderLeads() {
       </li>
     `
   }
-  // 3. Render the listItems inside the unordered list using ulEl.innerHTML
+  // Rendering the listItems inside the unordered list using ulEl.innerHTML
   ulEl.innerHTML = listItems
 }
